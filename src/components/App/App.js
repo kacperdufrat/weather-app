5222d8cfa0adeab3476a34ae6b4e6968594afa8d
unknown <kacper@dufrat.pl>
2020-02-24 11:05:43 +0100

    initial changes

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <p>
+      Weather app
+    </p>
+
+  );
+}
+
+export default App;
